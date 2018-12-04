dab712672b185e363b1c28cef9eddc783aac4252
zhubing <1483664313@qq.com>
2018-12-04 14:27:30 +0800

    完善生命周期

--- src/com/atguigu/cycle/Car.java
+++ src/com/atguigu/cycle/Car.java
@@ -26,4 +26,12 @@ public class Car {
     {
         System.out.println("destroy ...");
     }
+
+    @Override
+    public String toString()
+    {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
 }
